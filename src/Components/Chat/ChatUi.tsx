99a695b940b662acf2e0411886dc8e21040bda82
firoozmandy <maryam.firoozmandi@gmail.come>
2025-03-05 10:56:18 +0330

    editing

--- src/Components/Chat/ChatUi.tsx
+++ src/Components/Chat/ChatUi.tsx
@@ -1,7 +1,9 @@
 import ChatInput from "./ChatInput";
+import Messages from "./Messages";
 const ChatUI = () => {
   return (
     <div className="flex flex-col justify-between h-full p-4">
+      <Messages/>
       <ChatInput />
     </div>
   );
